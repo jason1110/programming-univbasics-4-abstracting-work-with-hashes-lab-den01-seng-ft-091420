ef4787bae5bee9728308004db08edfce079b1d19
Jason Easterly <jason.easterly11@gmail.com>
2020-08-19 23:33:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,7 @@ def my_hash_creator(key, value)
  new_hash[key] = value
 end
 
-p my_hash_creator(:name, "henry")
+#p my_hash_creator(:name, "henry")
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
